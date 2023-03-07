c7d4edabe46c24b3b25b12236768ff890a3a16af
Iron68A <84898382+Iron68A@users.noreply.github.com>
2023-03-07 18:14:37 +0100

    ajout projet

--- CM1/dev a rendre/exemple jeu/README.md
+++ CM1/dev a rendre/exemple jeu/README.md
@@ -1,2 +1,4 @@
 # WE4A-Web
 UV de Web , UTBM Primtemps 23, cours, TD et TP
+# Premier rendu 
+3 pages de bases avec leur CSS
---
+++ TD1/testphp.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Test PHP</title>
+</head>
+<body>
+
+<?php
+    $a = 1;
+    if ($a == 1) {
+?>
+<p>Bonjour !</p>
+
+<?php
+    }
+    else {
+?>
+<p>Salut !</p>
+<?php
+    }
+?>
+
+</body>
+</html>
